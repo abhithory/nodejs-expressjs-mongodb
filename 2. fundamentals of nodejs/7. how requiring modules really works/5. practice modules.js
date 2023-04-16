b8528647689b56286738e3ac6f96cc8427ef33e9
theteamisbest <abhithory@gmail.com>
2023-04-16 10:55:51 +0530

    fundamentals of nodejs

---
+++ 2. fundamentals of nodejs/7. how requiring modules really works/5. practice modules.js
@@ -0,0 +1,11 @@
+console.log(arguments);
+console.log(require("module").wrapper);
+
+// export
+// import
+
+// cashing
+require('./testModule')()
+require('./testModule')()
+require('./testModule')()
+require('./testModule')()
---
+++ 2. fundamentals of nodejs/7. how requiring modules really works/testModule.js
@@ -0,0 +1,5 @@
+console.log("this for cashing. it will only called once");
+
+module.exports = () => {
+    console.log("test");
+}
