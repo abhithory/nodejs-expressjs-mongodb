7d56839f970992bffe01674f8c4b6f45d70f409c
theteamisbest <abhithory@gmail.com>
2023-05-08 23:51:07 +0530

    env updated

--- 4. expresss/5. Project/server.js
+++ 4. expresss/5. Project/server.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const dotenv = require("dotenv");
-dotenv.config({ path: "./config.env" });
+dotenv.config();
 // this config should be above app require
 
 const app = require("./6app");
