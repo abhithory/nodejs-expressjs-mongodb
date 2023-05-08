c8dcc3de5c6c06aeeb58a4a4c659317c8a51a29a
theteamisbest <abhithory@gmail.com>
2023-05-08 19:19:17 +0530

    setup mongodb

---
+++ 5. mongodb/3. mongodb commands.js
@@ -0,0 +1,8 @@
+//  install mongodb server,mongosh (mongo shell) and compass
+
+// add in enviroment variables the path of bin folder of mongodb
+
+// run mongod for mongo server start
+// run mongosg for mongo shell
+
+
