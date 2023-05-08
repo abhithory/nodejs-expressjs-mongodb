31a23585d1d75f583ea9e5ac23bc95eca6078742
theteamisbest <abhithory@gmail.com>
2023-05-08 19:31:30 +0530

    basic mongodb comands

--- 5. mongodb/3. mongodb commands.js
+++ 5. mongodb/3. mongodb commands.js
@@ -6,3 +6,20 @@
 // run mongosg for mongo shell
 
 
+
+// show all the databases
+// > show dbs
+
+// create database or switch to a already exiting databasse
+// > use datbase-name
+
+// see all collections in db
+// show collections
+
+// creating collection and inserting document in that
+// db.tours.insertOne({name:"The forest hiker",price:297,rating:4.7})
+
+// inserting multipal documents
+// db.tours.insertMany([{name:"the sea explorer",price:497,rating:4.8},{name:"the snow adventurer",price:997,rating:4.9,difficulty:"easy"}])
+
+
