6fdfac61b21b366b7c691cf85c245af9c7591765
theteamisbest <abhithory@gmail.com>
2023-05-08 23:01:26 +0530

    creating a scheme and model

--- 4. expresss/5. Project/server.js
+++ 4. expresss/5. Project/server.js
@@ -20,7 +20,36 @@ const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSW
 mongoose.connect(DB).then(con => {
   // console.log(con.connection);
   console.log("Connected succefully");
+}).catch(err => {
+  console.log("Error while connecting to DB", err);
 })
+
+
+const tourSchema = new mongoose.Schema({
+  // name:String,
+  name: {
+    type: String,
+    // required:true,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+})
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+
+
+
+
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
