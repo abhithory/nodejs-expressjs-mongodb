673dde79cb2a8fe78cbd9cdde3f23561da13cdb4
theteamisbest <abhithory@gmail.com>
2023-05-09 23:48:05 +0530

    aggrigate middleware

--- 4. expresss/5. Project/Model/tourModel.js
+++ 4. expresss/5. Project/Model/tourModel.js
@@ -103,5 +103,10 @@ tourSchema.post(/^find/, function (docs, next) {
 })
 
 
+tourSchema.pre('aggregate', function (next) {
+    this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+    next();
+})
+
 const Tour = mongoose.model('Tour', tourSchema);
 module.exports = Tour;
