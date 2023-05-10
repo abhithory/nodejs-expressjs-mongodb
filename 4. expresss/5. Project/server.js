5911e1767745728e41a5a8529c32762f0caa172f
theteamisbest <abhithory@gmail.com>
2023-05-10 14:13:18 +0530

    handle unhandled promise rejection

--- 4. expresss/5. Project/server.js
+++ 4. expresss/5. Project/server.js
@@ -21,16 +21,23 @@ const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSW
 mongoose.connect(DB).then(con => {
   // console.log(con.connection);
   console.log("Connected succefully");
-}).catch(err => {
-  console.log("Error while connecting to DB", err);
 })
 
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log("Listening on port 3000");
 });
 
 // for eslint and prettier
 // npm i eslint prettier eslint-config-prettier eslint-plugin-prettier eslint-config-airbnb eslint-plugin-node eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react --save-dev
 
+
+process.on("unhandledRejection", err => {
+  console.log(err.name, err.message);
+  console.log("Unhandled rejection! Shutting down...");
+
+  server.close(() => {
+    process.exit(1)
+  })
+})
