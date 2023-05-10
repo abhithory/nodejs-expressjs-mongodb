4f2ef6fdff338fd777805f6e0802d07dcaa5e868
theteamisbest <abhithory@gmail.com>
2023-05-10 18:28:15 +0530

    creating user fixed

--- 4. expresss/5. Project/controllers/authController.js
+++ 4. expresss/5. Project/controllers/authController.js
@@ -4,7 +4,12 @@ const User = require('../Model/userModel');
 
 
 exports.signup = catchAsync(async (req, res, next) => {
-    const newUser = await User.create(req.body);
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm
+    });
     res.status(201).json({
         status: 'success',
         data: newUser
