b9da605fdbf2af30fb62a2733f11e90d988eed08
theteamisbest <abhithory@gmail.com>
2023-05-10 11:03:34 +0530

    refactring code for error: error class + error controller

--- 4. expresss/5. Project/6app.js
+++ 4. expresss/5. Project/6app.js
@@ -4,6 +4,8 @@ const fs = require("fs");
 const morgan = require("morgan");
 const tourRoutes = require('./routes/tourRoutes');
 const userRoutes = require('./routes/userRoutes');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 
 const app = express();
@@ -26,27 +28,12 @@ app.use("/api/v1/tours", tourRoutes)
 app.use("/api/v1/users", userRoutes)
 
 app.all("*", (req, res, next) => {
-    // return res.status(404).json({
-    //     status: "failer", data: `this ${req.originalUrl} route not defined`
-    // })
-
-    const err = new Error(`this ${req.originalUrl} route not defined`);
-    err.status = "fail";
-    err.statusCode = 404;
-    next(err);
+    next(new AppError(`this ${req.originalUrl} route not defined`, 404));
 })
 
 
 // error handling route
-app.use((err, req, res, next) => {
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || "error";
-
-    return res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    })
-})
+app.use(globalErrorHandler)
 
 
 module.exports = app;
