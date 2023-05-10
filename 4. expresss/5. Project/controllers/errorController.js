68c0e214e084020e55c83032131d2eb1eaac6086
theteamisbest <abhithory@gmail.com>
2023-05-10 13:24:42 +0530

    duplicate value errors

--- 4. expresss/5. Project/controllers/errorController.js
+++ 4. expresss/5. Project/controllers/errorController.js
@@ -6,6 +6,12 @@ const handleCastErrorDB = err => {
     return new AppError(message, 400);
 }
 
+const handleDuplicateFieldsDB = err => {
+    const value = err.errmsg.match(/"((?:\\.|[^"\\])*)"/)[0];
+    const message = `Duplicate field value: ${value}. Please use another value`;
+    return new AppError(message, 400);
+}
+
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
@@ -40,6 +46,8 @@ const globalErrorHandler = (err, req, res, next) => {
     } else if (process.env.NODE_ENV === 'production') {
         let error = { ...err }
         if (err.name === 'CastError') error = handleCastErrorDB(error);
+
+        if (err.code === 11000) error = handleDuplicateFieldsDB(err)
 
         sendErrorProd(error, res);
     }
