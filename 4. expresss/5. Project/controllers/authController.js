ce27bc6c99e53a874022de4c0a65b7e327e7f714
theteamisbest <abhithory@gmail.com>
2023-05-10 18:36:16 +0530

    creating jwt tokens on signup

--- 4. expresss/5. Project/controllers/authController.js
+++ 4. expresss/5. Project/controllers/authController.js
@@ -1,6 +1,6 @@
 const catchAsync = require("../utils/catchAsync");
-
 const User = require('../Model/userModel');
+const jwt = require('jsonwebtoken');
 
 
 exports.signup = catchAsync(async (req, res, next) => {
@@ -10,8 +10,15 @@ exports.signup = catchAsync(async (req, res, next) => {
         password: req.body.password,
         passwordConfirm: req.body.passwordConfirm
     });
+
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES
+    });
+
+
     res.status(201).json({
         status: 'success',
+        token,
         data: newUser
     })
 })
